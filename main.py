d27582ce6d4abb45a28f39e4ade9cad497957c71
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-01 16:08:19 -0300

    fix(main): Rename cache provider and update initialization - Replace SimpleCache initialized with Deta key with DetaProvider for a more   efficient caching strategy using Deta store. - Update the initialization of SimpleCache to use the new provider for better   cache management and improve performance. - Remove redundant code blocks and streamline the cache set operation by setting   a key with a string value "Value".

--- main.py
+++ main.py
@@ -1,47 +1,8 @@
 from simple_cache import SimpleCache
+from simple_cache.providers import DetaProvider
 from config import deta_key
 
-cache = SimpleCache(deta_key=deta_key)
-"""
-OR
+provider = DetaProvider(deta_key=deta_key)
+cache = SimpleCache(provider=provider)
 
-cache = SimpleCache()
-cache.init(deta_key=deta_key)
-"""
-
-# -*-*-*
-
-
-def generate_content() -> str:
-    return "content"
-
-
-# -*-*-*
-
-res = cache.get(key="/", action=generate_content)
-print(res)  # <CacheData value="content" valid=True>
-
-# -*-*-*
-
-res = cache.get(key="/", action=generate_content)
-print(res.valid)  # True
-
-# -*-*-*
-
-res = cache.get(key="/", action=generate_content)
-print(res.value)  # content
-
-# -*-*-*
-
-res = cache.set(key="/", value="<h1>Olá, Mundo!</h1>")
-
-print(res.value)  # <h1>Olá, Mundo!</h1>
-print(res.valid)  # True
-
-# -*-*-*
-
-cache.set_validate(key="/", valid=False)
-res = cache.get(key="/", action=generate_content)
-
-print(res.value)  # content
-print(res.valid)  # True
+cache.set(key="/", value="Value")
