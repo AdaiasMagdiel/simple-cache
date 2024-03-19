4e31fbdd4945b9f3dea2cc46a4e7c55f73a65f07
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-18 23:46:51 -0300

    style(provider): Add documentation to DetaProvider class
    Adds detailed docstrings to the DetaProvider class methods and attributes
    to improve code readability and maintainability.
    - Docstrings provide information on method purpose, arguments, and exceptions.
    - Enhances understanding of class functionalities for future reference.

--- simple_cache/providers/deta.py
+++ simple_cache/providers/deta.py
@@ -6,6 +6,15 @@ from simple_cache.providers.provider import Provider
 
 
 class DetaProvider(Provider):
+    """
+    A class that provides caching functionality using Deta as the backend.
+
+    Attributes:
+        cache_table (str): The name of the cache table in Deta.
+        deta (Deta): The Deta client instance.
+        cache_db (_Base): The Deta Base instance for the cache table.
+    """
+
     cache_table = "sc_cache"
     deta: Deta
     cache_db: _Base
@@ -15,6 +24,17 @@ class DetaProvider(Provider):
         deta_key: Optional[str] = None,
         table_name: Optional[str] = None
     ) -> None:
+        """
+        Initialize the DetaProvider with a given Deta key and table name.
+
+        Args:
+            deta_key (Optional[str]): The Deta key for authentication.
+            table_name (Optional[str]): The name of the cache table in Deta.
+
+        Raises:
+            ValueError: If the Deta key or table name is an empty string.
+        """
+
         if table_name == "":
             raise ValueError(
                 "The table name should be a valid name, not a empty string"
@@ -33,6 +53,17 @@ class DetaProvider(Provider):
             self.__configure_db()
 
     def init(self, **kwargs) -> None:
+        """
+        Initialize the DetaProvider with a given Deta key and table name.
+
+        Args:
+            deta_key (Optional[str]): The Deta key for authentication.
+            table_name (Optional[str]): The name of the cache table in Deta.
+
+        Raises:
+            ValueError: If the Deta key or table name is an empty string or None.
+        """
+
         table_name = kwargs.get('table_name', self.cache_table)
         deta_key = kwargs.get('deta_key')
 
@@ -52,6 +83,10 @@ class DetaProvider(Provider):
         self.__configure_db()
 
     def __configure_db(self):
+        """
+        Configure the Deta client and the cache database.
+        """
+
         self.deta = Deta(self.deta_key)
         self.cache_db = self.deta.Base(self.cache_table)
 
@@ -61,6 +96,22 @@ class DetaProvider(Provider):
         action: Callable[[], str],
         expire_in: Optional[timedelta] = None
     ) -> CacheData:
+        """
+        Retrieve a value from the cache.
+
+        Args:
+            key (str): The key to retrieve the value from the cache.
+            action (Callable[[], str]): A callable that returns the value to be cached.
+            expire_in (Optional[timedelta]): The time after which the cached value expires.
+
+        Returns:
+            CacheData: The cached data.
+
+        Raises:
+            ValueError: If the key is None or empty.
+            TypeError: If 'expire_in' is not a 'timedelta'.
+        """
+
         if not key:
             raise ValueError("Key can not be None or empty")
 
@@ -91,6 +142,22 @@ class DetaProvider(Provider):
         value: Any,
         expire_in: Optional[timedelta] = None
     ) -> CacheData:
+        """
+        Store a value in the cache.
+
+        Args:
+            key (str): The key to store the value in the cache.
+            value (Any): The value to be cached.
+            expire_in (Optional[timedelta]): The time after which the cached value expires.
+
+        Returns:
+            CacheData: The cached data.
+
+        Raises:
+            ValueError: If the key is None or empty.
+            TypeError: If 'expire_in' is not a 'timedelta'.
+        """
+
         if not key:
             raise ValueError("Key can not be None or empty")
 
@@ -113,6 +180,18 @@ class DetaProvider(Provider):
         return CacheData(value=value, valid=True)
 
     def set_validate(self, key: str, valid: bool, silent: bool = True) -> None:
+        """
+        Set the validation status of a cached value.
+
+        Args:
+            key (str): The key of the cached value.
+            valid (bool): Whether the cached value is valid.
+            silent (bool): Whether to suppress errors.
+
+        Raises:
+            ValueError: If the key is None or empty.
+        """
+
         if not key:
             raise ValueError("Key can not be None or empty")
 
